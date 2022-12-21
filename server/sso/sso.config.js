23e5d956a9ad2e2b035e9dd19f7b143db47e73f8
Luca Zampetti <lzampetti@gmail.com>
2022-12-21 09:00:07 +0100

    Update sso.config.js

--- server/sso/sso.config.js
+++ server/sso/sso.config.js
@@ -1,6 +1,6 @@
 const { readFileSync } = require('../core/utils/utils');
 
-const SSO_PRIVATE_KEY = process.env.SSO_PRIVATE_KEY || readFileSync(__dirname, './sso.key');
+const SSO_PRIVATE_KEY = (process.env.SSO_PRIVATE_KEY || readFileSync(__dirname, './sso.key')).replace(/\\n/g, '\n');
 
 const config = {
 	sso: {
