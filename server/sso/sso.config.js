16153dc40800f3019ce9b795eb3a2d17539b95e2
Luca Zampetti <lzampetti@gmail.com>
2022-12-21 11:38:31 +0100

    vercel

--- server/sso/sso.config.js
+++ server/sso/sso.config.js
@@ -1,10 +1,11 @@
 const { readFileSync } = require('../core/utils/utils');
 
+const SSO_ISSUER = process.env.SSO_ISSUER || 'bhere-sso';
 const SSO_PRIVATE_KEY = (process.env.SSO_PRIVATE_KEY || readFileSync(__dirname, './sso.key')).replace(/\\n/g, '\n');
 
 const config = {
 	sso: {
-		issuer: 'bhere-sso',
+		issuer: SSO_ISSUER,
 		privateKey: SSO_PRIVATE_KEY,
 	}
 };
